c4e1a715e28d0ab71bfe80e60d5a244192ef0daf
Munem <munem.soufi@proton.me>
2024-07-26 10:49:46 +0200

    add baseURL variable

--- client-dev/src/components/reuseable/fetchData.jsx
+++ client-dev/src/components/reuseable/fetchData.jsx
@@ -1,11 +1,21 @@
 export const postDate = async (path, data) => {
-  const response = await fetch(`http://localhost:5500/${path} || https://kiez-server.onrender.com/${path}`, {
+  const baseUrl = process.env.NODE_ENV === 'development'
+    ? process.env.REACT_APP_API_URL_LOCAL
+    : process.env.REACT_APP_API_URL_PROD;
+
+  const response = await fetch(`${baseUrl}/${path}`, {
     method: "POST",
     headers: {
-      "content-type": "application/json",
+      "Content-Type": "application/json",
     },
     credentials: "include",
     body: JSON.stringify(data),
   });
+
+  if (!response.ok) {
+    const errorData = await response.json();
+    throw new Error(errorData.message || 'An error occurred');
+  }
+
   return await response.json();
 };
