461bdfbc3ae20f5f9f106d0365073ff4faf543fa
Munem <munem.soufi@proton.me>
2024-07-30 14:37:19 +0200

    remove dotenv

--- client-dev/vite.config.js
+++ client-dev/vite.config.js
@@ -1,15 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'process.env': {
-      VITE_API_URL_LOCAL: process.env.VITE_API_URL_LOCAL,
-      VITE_API_URL_PROD: process.env.VITE_API_URL_PROD
-    }
-  }
 });
