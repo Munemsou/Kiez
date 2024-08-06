e29d37e9e7b39f1e67ffad151b2d8471a565e1e3
Munem <munem.soufi@proton.me>
2024-08-06 14:15:50 +0200

    add log to see if the isDevelopment veriable is  also on deployed/production true

--- client-dev/src/utils/envUtils.js
+++ client-dev/src/utils/envUtils.js
@@ -3,6 +3,7 @@
 // Determine the base URL based on the environment
 export const getBaseUrl = () => {
     const isDevelopment = import.meta.env.MODE === 'development';
+    console.log("isDevelopment:",isDevelopment);
     return isDevelopment
       ? import.meta.env.VITE_API_URL_LOCAL
       : import.meta.env.VITE_API_URL_PROD;
