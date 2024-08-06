1be93fcb42dc833c220c17d6c61dea64d72c26b1
Munem <munem.soufi@proton.me>
2024-08-06 15:04:12 +0200

    change MODE variable to production

--- client-dev/src/utils/envUtils.js
+++ client-dev/src/utils/envUtils.js
@@ -2,10 +2,9 @@
 
 // Determine the base URL based on the environment
 export const getBaseUrl = () => {
-    const isDevelopment = import.meta.env.MODE === 'development';
-    console.log("isDevelopment:",isDevelopment);
-    return isDevelopment
-      ? import.meta.env.VITE_API_URL_LOCAL
-      : import.meta.env.VITE_API_URL_PROD;
-  };
-  
+  const isProduction = import.meta.env.MODE === "production";
+  console.log("isProduction:", isProduction);
+  return isProduction
+    ? import.meta.env.VITE_API_URL_PROD
+    : import.meta.env.VITE_API_URL_LOCAL;
+};
