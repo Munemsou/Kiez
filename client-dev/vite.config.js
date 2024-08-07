ff6392bb3afc810677bd6e8c0d5849e397d92cb7
Munem <munem.soufi@proton.me>
2024-08-07 12:29:10 +0200

    Configuration for different environments

--- client-dev/vite.config.js
+++ client-dev/vite.config.js
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  // Configuration for different environments
   define: {
-    'process.env': process.env
-  }
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+  },
+  // More options as needed
 });
