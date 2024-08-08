5d6ab54308affdac72c8230a02fc74a9526bd6e4
Munem <munem.soufi@proton.me>
2024-08-08 13:56:59 +0200

    specific build envUtils

--- client-dev/src/utils/envUtils.js
+++ client-dev/src/utils/envUtils.js
@@ -4,6 +4,8 @@ export const getBaseUrl = () => {
   const isDevelopment = import.meta.env.MODE === 'development';
   console.log("Environment Mode:", import.meta.env.MODE); // Debug log
   console.log("Is Development:", isDevelopment); // Debug log
+  console.log(import.meta.env); // This will show all environment variables
+
   return isDevelopment
     ? import.meta.env.VITE_API_URL_LOCAL
     : import.meta.env.VITE_API_URL_PROD;
